53de5cf2fb32a46b120db469d1388d5acc9dd69f
SiC <nulled666@gmail.com>
2016-07-25 16:28:31 +0800

    minor

--- tinyq_test_prop.js
+++ tinyq_test_prop.js
@@ -2,28 +2,44 @@ require([
     'tinyq_test'
 ], function (do_test) {
 
-        var nodes = document.querySelectorAll('pre');
+    var nodes = document.querySelectorAll('pre');
 
-        do_test('add/remove class', 99,
-            function () {
-                return _q(nodes).class('passed failed test ok collapse')
-                    .class('-passed -failed -test -ok -collapse');
-            },
-            function () {
-                return $(nodes).addClass('passed failed test ok collapse')
-                    .removeClass('passed failed test ok collapse');
-            });
+    do_test('add class', 99,
+        function () {
+            return _q(nodes).class('passed failed test ok collapse');
+        },
+        function () {
+            return $(nodes).addClass('passed failed test ok collapse');
+        });
 
-        do_test('mixed action', 99,
-            function () {
-                return _q(nodes)
-                    .class('passed -run-code ^collapse')
-                    .class('-passed -collapse ^run-code ?run-code');
-            },
-            function () {
-                return $(nodes).addClass('passed').removeClass('run-code').toggleClass('collapse')
-                    .removeClass('passed collapse').toggleClass('run-code').hasClass('run-code');
-            });
-            
+    do_test('remove class', 99,
+        function () {
+            return _q(nodes).class('-passed -failed -test -ok -collapse');
+        },
+        function () {
+            return $(nodes).removeClass('passed failed test ok collapse');
+        });
+
+    do_test('add & remove', 99,
+        function () {
+            return _q(nodes).class('passed failed test ok collapse')
+                .class('-passed -failed -test -ok -collapse');
+        },
+        function () {
+            return $(nodes).addClass('passed failed test ok collapse')
+                .removeClass('passed failed test ok collapse');
+        });
+
+    do_test('mixed action', 99,
+        function () {
+            return _q(nodes)
+                .class('passed -run-code ^collapse')
+                .class('-passed -collapse ^run-code ?run-code');
+        },
+        function () {
+            return $(nodes).addClass('passed').removeClass('run-code').toggleClass('collapse')
+                .removeClass('passed collapse').toggleClass('run-code').hasClass('run-code');
+        });
+
 
 });
