528494210a20035b466d5dd40682e1fa1324cf60
SiC <nulled666@gmail.com>
2016-07-29 10:25:07 +0800

    minor

--- tinyq.test.js
+++ tinyq.test.js
@@ -11,21 +11,25 @@ define(function () {
 
         _each(loop, function () { y = func2(); })
         var t2 = _time('time002', false);
-        var color = t1 < t2 ? 'color: #090' : 'color: #900';
+        var color = t1 < t2 ? '[+]' : ':-:';
 
         var xl = x;
         var yl = y;
-        if(x && x.length != undefined) xl = x.length;
-        if(x && x.count != undefined) xl = x.count;
-        if(y && y.length != undefined) yl = y.length;
+        if (x && x.length != undefined) xl = x.length;
+        if (x && x.count != undefined) xl = x.count;
+        if (y && y.length != undefined) yl = y.length;
 
-        _log('%c' + tag + ' >', color, t1.toFixed(3), ':', t2.toFixed(3), '(' + xl + '/' + yl + ')', xl == yl);
+        _log(color, xl === yl, tag, parse_time(t1), ':', parse_time(t2), '[' + xl + '/' + yl + ']');
 
         if (show) {
             _log(x);
             _log(y);
         }
 
+    }
+
+    function parse_time(t){
+        return parseFloat(t.toFixed(3));
     }
 
     return do_test;
