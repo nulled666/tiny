dd2e3cf1247f64cf2ebc6bc59efa2dcc018937b3
SiC <nulled666@gmail.com>
2019-02-01 16:21:01 +0800

    + _message.register()

--- src/tiny.message.js
+++ src/tiny.message.js
@@ -10,6 +10,7 @@ define([
     //////////////////////////////////////////////////////////
 
     var _message = {
+        register: register_messages,
         listen: listen_message,
         post: post_message,
         postDelayed: post_delayed_message
@@ -21,13 +22,42 @@ define([
 
 
     var TAG_SUFFIX = G.TAG_SUFFIX;
-    var SEE_ABOVE = G.SEE_ABOVE;
-    var _error = tiny.error;
-
 
     // internal registry of message handlers
     var _message_handlers = {};
     var _delayed_messages = {};
+
+    var TAG_MSG_REGISTER = '_message.register()' + TAG_SUFFIX;
+    /**
+     * Register messages before using them
+     */
+    function register_messages(namespace, messages) {
+
+        if (messages == undefined) {
+            messages = namespace
+            namespace = ''
+        }
+
+        if (typeof namespace !== 'string')
+            throw new TypeError(TAG_MSG_REGISTER + 'Message namespace must be a string. > Got "' + typeof namespace + '"')
+
+        if (namespace != '') namespace += '::'
+        if (typeof messages === 'string') messages = [messages]
+
+        if (tiny.type(messages) !== 'Array')
+            throw new TypeError(TAG_MSG_REGISTER + 'Message names must be a string or array. > Got "' + typeof messages + '"')
+
+        tiny.each(messages, function (msg) {
+            let key = namespace + msg
+            if (_message_handlers[key] !== undefined)
+                throw new TypeError(TAG_MSG_REGISTER + 'Message has already been registered: "' + key + '"')
+            _message_handlers[key] = []
+        })
+
+        return _message
+
+    }
+
 
     var TAG_MSG_LISTEN = '_message.listen()' + TAG_SUFFIX;
     /**
@@ -40,18 +70,14 @@ define([
      */
     function listen_message(msg, handler) {
 
-        if (typeof msg !== 'string') {
-            _error(TAG_MSG_LISTEN, 'Expect a message string. > Got "' + typeof msg + '": ', msg);
-            throw new TypeError(SEE_ABOVE);
-        }
+        if (typeof msg !== 'string')
+            throw new TypeError(TAG_MSG_LISTEN + 'Expect a message string. > Got "' + typeof msg + '"')
 
-        if (typeof handler !== 'function') {
-            _error(TAG_MSG_LISTEN, 'Expect a function. > Got "' + typeof handler + '": ', handler);
-            throw new TypeError(SEE_ABOVE);
-        }
+        if (typeof handler !== 'function')
+            throw new TypeError(TAG_MSG_LISTEN + 'Expect a function. > Got "' + typeof handler + '"')
 
         if (!_message_handlers[msg])
-            _message_handlers[msg] = [];
+            throw new Error(TAG_MSG_LISTEN + 'Message has not been registered: "' + msg + '"')
 
         tiny.log(TAG_MSG_LISTEN + '"' + msg + '" => ', handler.name + '()');
 
@@ -60,6 +86,7 @@ define([
         return _message;
 
     }
+
 
     var TAG_MSG_POST = '_message.post()' + TAG_SUFFIX;
     /**
@@ -70,26 +97,24 @@ define([
      */
     function post_message(msg) {
 
-        if (typeof msg !== 'string') {
-            _error(TAG_MSG_POST, 'Expect a message string. > Got "' + typeof msg + '": ', msg);
-            throw new TypeError(SEE_ABOVE);
-        }
+        if (typeof msg !== 'string')
+            throw new TypeError(TAG_MSG_POST + 'Expect a message string. > Got "' + typeof msg + '"')
 
         var sliced_args = tiny.x.toArray(arguments, 1)
 
         tiny.group(TAG_MSG_POST + '"' + msg + '"')
 
-        var handles = _message_handlers[msg]
+        var handlers = _message_handlers[msg]
 
-        if (!handles || handles.length < 1) {
+        if (!handlers || handlers.length < 1) {
             tiny.warn(TAG_MSG_POST, 'Nobody is listen to this message')
             return _message
         }
 
         // call handles
-        tiny.each(handles, function (callback) {
+        tiny.each(handlers, function (callback) {
             callback.apply(this, sliced_args)
-        });
+        })
 
         tiny.group()
 
@@ -106,25 +131,22 @@ define([
      */
     function post_delayed_message(delay, msg) {
 
-        if (typeof delay !== 'number') {
-            _error(TAG_MSG_POST_DELAYED, 'Expect a delay time in milliseconds. > Got "' + typeof delay + '": ', delay);
-            throw new TypeError(SEE_ABOVE);
-        }
+        if (typeof delay !== 'number')
+            throw new TypeError(TAG_MSG_POST_DELAYED + 'Expect a delay time in milliseconds. > Got "' + typeof delay + '"');
 
-        if (typeof msg !== 'string') {
-            _error(TAG_MSG_POST_DELAYED, 'Expect a message string. > Got "' + typeof msg + '": ', msg);
-            throw new TypeError(SEE_ABOVE);
-        }
+        if (typeof msg !== 'string')
+            throw new TypeError(TAG_MSG_POST_DELAYED + 'Expect a message string. > Got "' + typeof msg + '"');
 
-        var sliced_args = tiny.x.toArray(arguments, 1);
+        var sliced_args = tiny.x.toArray(arguments, 1)
+
         if (_delayed_messages[msg] !== undefined)
-            clearTimeout(_delayed_messages[msg]);
+            clearTimeout(_delayed_messages[msg])
 
         _delayed_messages[msg] = setTimeout(function () {
-            post_message.apply(this, sliced_args);
-        }, delay);
+            post_message.apply(this, sliced_args)
+        }, delay)
 
-        return _message;
+        return _message
 
     }
 
